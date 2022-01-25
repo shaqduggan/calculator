a7330b064be837d097ec3377b99073dbc999c88c
shaquillecode <84408174+shaquillecode@users.noreply.github.com>
2022-01-25 11:58:31 -0500

    Added Subtract feature to Feature-sub branch

--- calculator-exercise.py
+++ calculator-exercise.py
@@ -8,9 +8,14 @@ class Calculator:
 	def add(self,x,y):
 		""" add's two numbers x,y together """
 		return x + y
+	
+	def subtract(self,x,y):
+		""" subtract's number y from number x  """
+		return x - y
 
 
 if __name__ == '__main__':
 	calc = Calculator()
 	print(calc.add(2,3))
+	print(calc.subtract(100000,20000))
 	
