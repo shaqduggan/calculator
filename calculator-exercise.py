a79c38280582763eb1e9b5a3f64f6a9781df0628
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-01-25 12:19:46 -0500

    Update calculator-exercise.py

--- calculator-exercise.py
+++ calculator-exercise.py
@@ -1,3 +1,4 @@
+import math
 
 class Calculator:
 
@@ -10,7 +11,23 @@ class Calculator:
 		return x + y
 
 
+	def sine(self,x):
+		'''Finds the sine of the value passed in'''
+		return math.sin(x)
+
+	def cosine(self,x):
+		'''Finds the cosine of the value passed in'''
+		return math.cos(x)
+
+	def tangent(self,x):
+		'''Finds the tangent of the value passed in'''
+		return math.tan(x)
+
+
+
 if __name__ == '__main__':
 	calc = Calculator()
 	print(calc.add(2,3))
-	
+	print(calc.sine(2))
+	print(calc.cosine(2))
+	print(calc.tangent(2))
