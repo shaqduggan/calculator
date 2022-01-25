9fa2edf0bf163174e17d3fb12131edc395a31a92
Debby Cosoi <mariel.cosoi@codeimmersives.com>
2022-01-25 14:15:29 -0300

    division formula added

--- calculator-exercise.py
+++ calculator-exercise.py
@@ -9,8 +9,14 @@ class Calculator:
 		""" add's two numbers x,y together """
 		return x + y
 
+	
+	def division(self,x,y):
+		""" divide's two numbers x,y together """
+		return (x / y) if y != 0 else 0
+
 
 if __name__ == '__main__':
 	calc = Calculator()
 	print(calc.add(2,3))
+	print(calc.division(2,3))
 	
