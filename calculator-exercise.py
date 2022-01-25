062fc81493d33f68a7b15a160ab6b8b7b52450f0
ITO-Taro <84404661+ITO-Taro@users.noreply.github.com>
2022-01-25 14:43:44 -0500

    added function to find median

--- calculator-exercise.py
+++ calculator-exercise.py
@@ -36,6 +36,12 @@ class Calculator:
 	def division(self,x,y):
 		""" divide's two numbers x,y together """
 		return (x / y) if y != 0 else 0
+	
+	def find_median(self, x):
+		"""finds the center most value"""
+		x.sort()
+		n = int(len(x)/2)
+		return round((x[n]+x[n+1])/2, 2) if len(x)%2 == 0 else round(x[int((len(x)-1)/2)], 2)
 
 
 if __name__ == '__main__':
@@ -48,3 +54,4 @@ if __name__ == '__main__':
 	print(calc.cosine(2))
 	print(calc.tangent(2))
 	print(calc.subtract(100000,20000))
+	print(calc.find_median([1,1,9,7,5,1,3,2,6,4,8,9,4,2,3,1,5]))
