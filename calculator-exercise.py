6ef310c48bedf22fe0e37eff638da0cacc43cae7
ITO-Taro <84404661+ITO-Taro@users.noreply.github.com>
2022-01-25 12:12:39 -0500

    added remainder function

--- calculator-exercise.py
+++ calculator-exercise.py
@@ -12,10 +12,14 @@ class Calculator:
 	def subtract(self,x,y):
 		""" subtract's number y from number x  """
 		return x - y
+	
+	def calc_remainder(x, n):
+		return x % n
 
 
 if __name__ == '__main__':
 	calc = Calculator()
 	print(calc.add(2,3))
 	print(calc.subtract(100000,20000))
+	print(calc.calc_remainder(7,3))
 	
