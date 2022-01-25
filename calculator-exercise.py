1983df36f0f880251389cb39b3d451e526fbf302
dd-code-immersives <domilamad89@gmail.com>
2022-01-25 17:39:41 +0100

    committing the new file

---
+++ calculator-exercise.py
@@ -0,0 +1,16 @@
+
+class Calculator:
+
+	""" 
+	Calculator module for in class assignment 
+	"""
+
+	def add(self,x,y):
+		""" add's two numbers x,y together """
+		return x + y
+
+
+if __name__ == '__main__':
+	calc = Calculator()
+	print(calc.add(2,3))
+	
