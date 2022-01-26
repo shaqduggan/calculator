429abea16bb6ca5f911d02ee77bd4a906083612c
ITO-Taro <84404661+ITO-Taro@users.noreply.github.com>
2022-01-26 17:59:47 -0500

    added test remainder

---
+++ tests/test_remainder.py
@@ -0,0 +1,17 @@
+import unittest
+from calculator_exercise import Calculator
+
+class CalculatorTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.calc = Calculator()
+
+    def test_remainder_integer(self):
+        self.assertEqual(self.calc.remainder(7, 4), 3)
+    
+    def test_remainder_float(self):
+        self.assertFalse(isinstance(self.calc.remainder(10, 0.3), int))
+
+if __name__ == '__main__':
+    unittest.main()
