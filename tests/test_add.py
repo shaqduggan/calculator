bcdf1d3924a6b72911ef4d3fce223fd6de36b629
dd-code-immersives <domilamad89@gmail.com>
2022-01-26 20:58:54 +0100

    added fixture for class

--- tests/test_add.py
+++ tests/test_add.py
@@ -3,14 +3,24 @@ from calculator import Calculator
 
 class CalculatorTests(unittest.TestCase):
 
+	@classmethod
+	def setUpClass(cls):
+		"""
+		instantiate our class  (fixture)
+		"""
+		cls.calc = Calculator()
 
 	def test_add_integers(self):
-		calc = Calculator()
-		self.assertEqual(calc.add(2,3), 5)
+		""" 
+		Test the addition of integers
+		"""
+		self.assertEqual(self.calc.add(2,3), 5)
 
 	def test_add_floats(self):
-		calc = Calculator()
-		self.assertEqual(calc.add(2.0,3.2), 5.2)
+		""" 
+		Test the addition of floats
+		"""
+		self.assertEqual(self.calc.add(2.0,3.2), 5.2)
 
 if __name__ == '__main__':
     unittest.main()
