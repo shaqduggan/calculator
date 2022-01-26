2bd24bdafdbd160acfef425c08c110f6e6670516
dd-code-immersives <domilamad89@gmail.com>
2022-01-26 17:39:32 +0100

    Revert "added 3 param handling"
    This reverts commit 82b98a7a6045bb8040535c4d7b4cb29c6d1322b2.

--- calculator-exercise.py
+++ calculator-exercise.py
@@ -5,9 +5,9 @@ class Calculator:
 	Calculator module for in class assignment 
 	"""
 
-	def add(self,x,y,z):
+	def add(self,x,y):
 		""" add's two numbers x,y together """
-		return x + y + z
+		return x + y
 
 
 if __name__ == '__main__':
