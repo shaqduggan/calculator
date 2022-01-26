f3ab3d005fcdef67b44b7f96f2a3bea646c09d87
dd-code-immersives <domilamad89@gmail.com>
2022-01-26 17:37:35 +0100

    adding 3 param handling

--- calculator-exercise.py
+++ calculator-exercise.py
@@ -5,9 +5,9 @@ class Calculator:
 	Calculator module for in class assignment 
 	"""
 
-	def add(self,x,y):
+	def add(self,x,y, z):
 		""" add's two numbers x,y together """
-		return x + y
+		return x + y + z 
 
 
 if __name__ == '__main__':
