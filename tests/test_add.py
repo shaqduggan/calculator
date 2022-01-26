812d7d258ef96a85c702e83562ee66f0d92efeab
dd-code-immersives <domilamad89@gmail.com>
2022-01-26 21:45:33 +0100

    reverted commit

--- tests/test_add.py
+++ tests/test_add.py
@@ -22,11 +22,5 @@ class CalculatorTests(unittest.TestCase):
 		"""
 		self.assertEqual(self.calc.add(2.0,3.2), 5.2)
 
-	def test_multiply_integers(self):
-		"""
-		Test the multiplication of integers
-		"""
-		self.assertEqual(self.calc.multiplication(2, 3), 6)
-
 if __name__ == '__main__':
     unittest.main()
