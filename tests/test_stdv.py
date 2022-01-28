3e1ed39ac01a6c83be7ddc58cf0c13373d5b7a97
ITO-Taro <84404661+ITO-Taro@users.noreply.github.com>
2022-01-28 13:46:16 -0500

    defined the set of data as class variable

--- tests/test_stdv.py
+++ tests/test_stdv.py
@@ -2,7 +2,9 @@ import unittest
 import statistics
 from calculator import Calculator
 
+
 class CalculatorStdvTests(unittest.TestCase):
+    testData = [10,1,9,7,5,1,3.2,2,6,4,8,9,4.8,2,3,1,5,10,3]
 
     @classmethod
     def setUpClass(cls):
@@ -10,11 +12,11 @@ class CalculatorStdvTests(unittest.TestCase):
 
     def test_stdv(self):
         """ tests if the functuon returns stdv value as expected """
-        self.assertEqual(self.calc.stdv([10,1,9,7,5,1,3.2,2,6,4,8,9,4.8,2,3,1,5,10,3]), 3.1112)
+        self.assertEqual(self.calc.stdv(self.data), 3.1112)
 
     def test_stdv_against_stat_lib(self):
         """ comapare returned value from calc.stdv to the equivalence of stats lib (both rounded at 4th decimal)"""
-        self.assertEqual(self.calc.stdv([10,1,9,7,5,1,3.2,2,6,4,8,9,4.8,2,3,1,5,10,3]), round(statistics.stdev([10,1,9,7,5,1,3.2,2,6,4,8,9,4.8,2,3,1,5,10,3]), 4))
+        self.assertEqual(self.calc.stdv(self.testData), round(statistics.stdev(self.testData), 4))
 
 if __name__ == '__main__':
     unittest.main()
